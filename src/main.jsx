c1852b987d0c5cddb761ddf1db611a41953d9bb2
JamesC585 <92755364+JamesC585@users.noreply.github.com>
2025-05-23 06:16:11 -0700

    Remove

--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import App from './App.jsx';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
